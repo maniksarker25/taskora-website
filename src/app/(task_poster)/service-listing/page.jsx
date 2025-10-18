da39878634b216af694d72586d22067f3a96ffb3
ramohin1236 <mohinr26@gmail.com>
2025-10-18 15:45:38 +0600

    side nav

--- src/app/(task_poster)/service-listing/page.jsx
+++ src/app/(task_poster)/service-listing/page.jsx
@@ -157,7 +157,7 @@ const ServiceListing = () => {
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
                 <input
                   type="text"
-                  placeholder="Search your perfect event"
+                  placeholder="Search your service"
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
                   className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
