b1e6ef6ac66f87ab74d8c54c56e6b1d302594c82
manik <maniksarker652@gmail.com>
2026-03-23 21:15:21 +0600

    change base url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,6 @@
 export const baseUrl = () => {
-  return "http://localhost:9000/api/v1";
+  // return "http://localhost:9000/api/v1";
+  return "https://task-master-vwva.onrender.com/api/v1";
 };
 
 export default baseUrl;
